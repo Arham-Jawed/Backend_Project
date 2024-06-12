ec5ef33886df0c348a74208fa2733f8f4571e47d
Arham Jawed <arham0jawed@gmail.com>
2024-06-12 17:28:49 +0530

    modified in file

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 const connectDb = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`MongoDb Connected !! MongoDb Host Is : ${connectionInstance}`);
+        console.log(`MongoDb Connected !! MongoDb Host Is : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log(`MongoDb Connection Error : ${error}`);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 
-dotenv.config();
+dotenv.config({path : "./env"});
 connectDB();
 
 
