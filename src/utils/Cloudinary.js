fc95b4c9a342ee7b6328d8e26fb55e5698c01cfb
Arham Jawed <arham0jawed@gmail.com>
2024-06-13 19:03:32 +0530

    added multer and cloud files

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloud} from "cloudinary"
+import fs from "fs"
+
+cloud.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET, 
+});
+
+const uploadOnCloud = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        const response = await cloud.uploader.upload(localFilePath,{
+            resource_type : "auto"
+        })
+        console.log("File Is Uploaded Successfully On Cloud ", response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export { uploadOnCloud }
